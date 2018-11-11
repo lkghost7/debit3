3522dec8626853ef420ebbfcb76feafddfcbf3cf
lk <lkghost7@gmail.com>
2018-11-11 22:24:26 +0300

    удаление старых тестов

--- database/src/test/java/by/itacademy/dao/run/RunnableTest.java
+++ database/src/test/java/by/itacademy/dao/run/RunnableTest.java
@@ -1,6 +1,8 @@
 package by.itacademy.dao.run;
 
 import by.itacademy.dao.*;
+import by.itacademy.dao.generic.DebitorDaoImplTest;
+import by.itacademy.dao.generic.PrivilegeDaoImplTest;
 import by.itacademy.dao.generic.SystemUserDaoImplTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -8,14 +10,12 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
 
-        CompanyDaoTest.class,
         CompanDaoImplTest.class,
-        DebitorDaoTest.class,
-        EmailDetailDaoTest.class,
-        PersonDaoTest.class,
-        RegistryContractsDaoTest.class,
-        PrivilegeDaoImplTest.class,
         SystemUserDaoImplTest.class,
+        PrivilegeDaoImplTest.class,
+        DebitorDaoImplTest.class,
+        CompanDaoImplTest.class
+
 })
 public class RunnableTest {
 }
